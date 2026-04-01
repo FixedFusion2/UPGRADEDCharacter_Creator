7520ca35232ec8a2c64b3cf16b6c201b8a129ddf
FixedFusion2 <true.eggleston@ucas-edu.net>
2026-04-01 09:48:31 -0600

    matplotlib/pandas

--- character_creator/tests/matplotlib_test.py
+++ character_creator/tests/matplotlib_test.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from faker import *
+fake = Faker()
+name1 = fake.name()
+name2 = fake.name()
+name3 = fake.name()
+name4 = fake.name()
+name5 = fake.name()
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
 # Example data
-charcters = ('Thor Odenson', 'Tolkien Lewis', 'Harry Potter', 'Slim Jim', 'Jimithy Timson')
+charcters = (name1, name2, name3, name4, name5)
 y_pos = np.arange(len(charcters))
 performance = 3 + 10 * np.random.rand(len(charcters))
 error = np.random.rand(len(charcters))
@@ -15,8 +21,8 @@ fig, ax = plt.subplots()
 hbars = ax.barh(y_pos, performance, xerr=error, align='center')
 ax.set_yticks(y_pos, labels=charcters)
 ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Power')
-ax.set_title('Power of characters')
+ax.set_xlabel('Strength', fontsize = 30)
+ax.set_title('Strength of Characters', fontsize = 50)
 
 # Label with specially formatted floats
 ax.bar_label(hbars, fmt='%.2f')
